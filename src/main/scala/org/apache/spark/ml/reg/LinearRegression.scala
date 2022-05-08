775b1e54ba637636cf66fa379d9d0dc00c20e19d
Sergei Kozelko <kozelkos@yandex.ru>
2022-05-08 11:20:25 +0300

    Implement save and load

--- src/main/scala/org/apache/spark/ml/reg/LinearRegression.scala
+++ src/main/scala/org/apache/spark/ml/reg/LinearRegression.scala
@@ -1,6 +1,7 @@
 package org.apache.spark.ml.reg
 
 import org.apache.spark.ml.attribute.AttributeGroup
+import org.apache.spark.ml.linalg.BLAS.dot
 import org.apache.spark.ml.linalg.{DenseVector, Vector, VectorUDT, Vectors}
 import org.apache.spark.ml.param.shared.{HasInputCol, HasOutputCol}
 import org.apache.spark.ml.param.{BooleanParam, ParamMap}
@@ -8,7 +9,6 @@ import org.apache.spark.ml.regression.{RegressionModel, Regressor}
 import org.apache.spark.ml.util._
 import org.apache.spark.ml.{Estimator, Model, PredictorParams}
 import org.apache.spark.mllib
-import org.apache.spark.mllib.stat.MultivariateOnlineSummarizer
 import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 import org.apache.spark.sql.types.{DataType, StructType}
 import org.apache.spark.sql.{DataFrame, Dataset, Encoder}
@@ -22,14 +22,14 @@ class LinearRegression(override val uid: String) extends Regressor[Vector, Linea
 
   override def train(dataset: Dataset[_]): LinearRegressionModel = {
 
-//    // Used to convert untyped dataframes to datasets with vectors
-//    implicit val encoder : Encoder[Vector] = ExpressionEncoder()
-//
-//    val vectors: Dataset[Vector] = dataset.select(dataset($(featuresCol)).as[Vector])
-//
-//    val dim: Int = AttributeGroup.fromStructField((dataset.schema($(featuresCol)))).numAttributes.getOrElse(
-//      vectors.first().size
-//    )
+    // Used to convert untyped dataframes to datasets with vectors
+    implicit val encoder : Encoder[Vector] = ExpressionEncoder()
+
+    val vectors: Dataset[Vector] = dataset.select(dataset($(featuresCol)).as[Vector])
+
+    val dim: Int = AttributeGroup.fromStructField((dataset.schema($(featuresCol)))).numAttributes.getOrElse(
+      vectors.first().size
+    )
 //
 //    val summary = vectors.rdd.mapPartitions((data: Iterator[Vector]) => {
 //      val result = data.foldLeft(new MultivariateOnlineSummarizer())(
@@ -37,7 +37,9 @@ class LinearRegression(override val uid: String) extends Regressor[Vector, Linea
 //      Iterator(result)
 //    }).reduce(_ merge _)
 
-    copyValues(new LinearRegressionModel()).setParent(this)
+    val weights = Vectors.zeros(dim)
+
+    copyValues(new LinearRegressionModel(weights, bias = 0)).setParent(this)
 
     //    val Row(row: Row) =  dataset
     //      .select(Summarizer.metrics("mean", "std").summary(dataset($(inputCol))))
@@ -51,27 +53,29 @@ class LinearRegression(override val uid: String) extends Regressor[Vector, Linea
 
 object LinearRegression extends DefaultParamsReadable[LinearRegression]
 
-class LinearRegressionModel private[made](override val uid: String)
+class LinearRegressionModel private[reg](override val uid: String,
+                                          val weights: Vector,
+                                          val bias: Double)
     extends RegressionModel[Vector, LinearRegressionModel] with LinearRegressionParams with MLWritable {
 
-  private[made] def this() =
-    this(Identifiable.randomUID("LinearRegressionModel"))
+  private[reg] def this(weights: Vector, bias: Double) =
+    this(Identifiable.randomUID("LinearRegressionModel"), weights, bias)
 
   override def copy(extra: ParamMap): LinearRegressionModel = copyValues(
-    new LinearRegressionModel(), extra)
+    new LinearRegressionModel(weights, bias), extra)
 
   override def predict(features: Vector): Double = {
-    0.0
+    dot(features, weights) + bias
   }
 
   override def write: MLWriter = new DefaultParamsWriter(this) {
-//    override protected def saveImpl(path: String): Unit = {
-//      super.saveImpl(path)
-//
-//      val vectors = means.asInstanceOf[Vector] -> stds.asInstanceOf[Vector]
-//
-//      sqlContext.createDataFrame(Seq(vectors)).write.parquet(path + "/vectors")
-//    }
+    override protected def saveImpl(path: String): Unit = {
+      super.saveImpl(path)
+
+      val vectors = weights.asInstanceOf[Vector] -> bias.asInstanceOf[Double]
+
+      sqlContext.createDataFrame(Seq(vectors)).write.parquet(path + "/vectors")
+    }
   }
 }
 
@@ -80,14 +84,15 @@ object LinearRegressionModel extends MLReadable[LinearRegressionModel] {
     override def load(path: String): LinearRegressionModel = {
       val metadata = DefaultParamsReader.loadMetadata(path, sc)
 
-//      val vectors = sqlContext.read.parquet(path + "/vectors")
+      val vectors = sqlContext.read.parquet(path + "/vectors")
 
       // Used to convert untyped dataframes to datasets with vectors
-//      implicit val encoder : Encoder[Vector] = ExpressionEncoder()
+      implicit val encoder : Encoder[Vector] = ExpressionEncoder()
+      implicit val encoderD : Encoder[Double] = ExpressionEncoder()
 
-//      val (means, std) =  vectors.select(vectors("_1").as[Vector], vectors("_2").as[Vector]).first()
+      val (weights, bias) =  vectors.select(vectors("_1").as[Vector], vectors("_2").as[Double]).first()
 
-      val model = new LinearRegressionModel()
+      val model = new LinearRegressionModel(weights, bias)
       metadata.getAndSetParams(model)
       model
     }
--- src/test/scala/org/apache/spark/ml/reg/LinearRegressionTest.scala
+++ src/test/scala/org/apache/spark/ml/reg/LinearRegressionTest.scala
@@ -1,5 +1,77 @@
 package org.apache.spark.ml.reg
 
-class LinearRegressionTest {
+import com.google.common.io.Files
+import org.apache.spark.ml.linalg.{Vector, Vectors}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.sql.DataFrame
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
 
+class LinearRegressionTest extends AnyFlatSpec with should.Matchers with WithSpark {
+
+  lazy val data: DataFrame = LinearRegressionTest._data
+  lazy val vectors: Seq[(Vector, Double)] = LinearRegressionTest._vectors
+
+  "Estimator" should "calculate weights" in {
+    val estimator = new LinearRegression()
+      .setFeaturesCol("features")
+      .setPredictionCol("prediction")
+      .setLabelCol("label")
+
+    val model = estimator.fit(data)
+  }
+
+  "Estimator" should "work after re-read" in {
+
+    val pipeline = new Pipeline().setStages(Array(
+      new LinearRegression()
+        .setFeaturesCol("features")
+        .setPredictionCol("prediction")
+        .setLabelCol("label")
+    ))
+
+    val tmpFolder = Files.createTempDir()
+
+    pipeline.write.overwrite().save(tmpFolder.getAbsolutePath)
+
+    val reRead = Pipeline.load(tmpFolder.getAbsolutePath)
+
+    val model = reRead.fit(data).stages(0).asInstanceOf[LinearRegressionModel]
+
+//    model.means(0) should be(vectors.map(_(0)).sum / vectors.length +- delta)
+//    model.means(1) should be(vectors.map(_(1)).sum / vectors.length +- delta)
+//
+//    validateModel(model, model.transform(data))
+  }
+
+  "Model" should "work after re-read" in {
+
+    val pipeline = new Pipeline().setStages(Array(
+      new LinearRegression()
+        .setFeaturesCol("features")
+        .setPredictionCol("predictions")
+        .setLabelCol("label")
+    ))
+
+    val model = pipeline.fit(data)
+
+    val tmpFolder = Files.createTempDir()
+
+    model.write.overwrite().save(tmpFolder.getAbsolutePath)
+
+    val reRead: PipelineModel = PipelineModel.load(tmpFolder.getAbsolutePath)
+  }
+}
+
+object LinearRegressionTest extends WithSpark {
+
+  lazy val _vectors = Seq(
+    Tuple2(Vectors.dense(13.5, 12), 1.0),
+    Tuple2(Vectors.dense(-1, 0), 2.0),
+  )
+
+  lazy val _data: DataFrame = {
+    import sqlc.implicits._
+    _vectors.toDF("features", "label")
+  }
 }
