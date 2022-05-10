2065cfb36d8c88a0d687196923636ea115a6c50e
Sergei Kozelko <kozelkos@yandex.ru>
2022-05-10 10:38:08 +0300

    Refactor tests

--- src/test/scala/org/apache/spark/ml/reg/LinearRegressionTest.scala
+++ src/test/scala/org/apache/spark/ml/reg/LinearRegressionTest.scala
@@ -9,6 +9,7 @@ import org.scalatest.matchers.should
 
 class LinearRegressionTest extends AnyFlatSpec with should.Matchers with WithSpark {
 
+  val delta = 0.0001
   lazy val data: DataFrame = LinearRegressionTest._data
   lazy val vectors: Seq[(Vector, Double)] = LinearRegressionTest._vectors
 
@@ -23,9 +24,9 @@ class LinearRegressionTest extends AnyFlatSpec with should.Matchers with WithSpa
 
     model.weights.leftSide
 
-    model.weights(0) should be (1.0 +- 0.001)
-    model.weights(1) should be (2.0 +- 0.001)
-    model.bias should be(0.0 +- 0.001)
+    model.weights(0) should be (1.0 +- delta)
+    model.weights(1) should be (2.0 +- delta)
+    model.bias should be(0.0 +- delta)
   }
 
   "Estimator" should "should produce functional model" in {
@@ -37,15 +38,14 @@ class LinearRegressionTest extends AnyFlatSpec with should.Matchers with WithSpa
 
     val model = estimator.fit(data)
 
-    validateModel(model, model.transform(data))
+    validatePredictions(model.transform(data))
   }
 
-  private def validateModel(model: LinearRegressionModel, data: DataFrame) = {
+  private def validatePredictions(data: DataFrame) = {
     val vectors: Array[Double] = data.collect().map(_.getAs[Double]("predictions"))
 
     vectors.length should be(3)
 
-    val delta = 0.001
     vectors(0) should be(1.0 +- delta)
     vectors(1) should be(2.0 +- delta)
     vectors(2) should be(3.0 +- delta)
@@ -69,9 +69,9 @@ class LinearRegressionTest extends AnyFlatSpec with should.Matchers with WithSpa
 
     val model = reRead.fit(data).stages(0).asInstanceOf[LinearRegressionModel]
 
-    model.weights(0) should be (1.0 +- 0.001)
-    model.weights(1) should be (2.0 +- 0.001)
-    model.bias should be(0.0 +- 0.001)
+    model.weights(0) should be (1.0 +- delta)
+    model.weights(1) should be (2.0 +- delta)
+    model.bias should be(0.0 +- delta)
   }
 
   "Model" should "work after re-read" in {
@@ -92,7 +92,7 @@ class LinearRegressionTest extends AnyFlatSpec with should.Matchers with WithSpa
 
     val reRead: PipelineModel = PipelineModel.load(tmpFolder.getAbsolutePath)
 
-    validateModel(model.stages(0).asInstanceOf[LinearRegressionModel], reRead.transform(data))
+    validatePredictions(reRead.transform(data))
   }
 }
 
